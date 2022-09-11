1e71b84e2eef098b17a8c175ff44cb09cf8483c6
Sagar Maharjan <sagarmaharjan31@gmail.com>
2022-09-11 17:57:06 +0545

    configuration for openweathermap api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  weather_map_api_key: 'f52d0d68ae6436506468bcf625d9e8e1',
+  base_url: 'https://api.openweathermap.org/data/2.5/',
 };
